ad6a402e3fd379c20bb5742e835cceb2720beca4
Irez Abdullah <irezabdullah18@gmail.com>
2025-07-21 07:11:38 +0700

    fix: color text on room status

--- resources/views/user/room/index.blade.php
+++ resources/views/user/room/index.blade.php
@@ -79,7 +79,7 @@
                                     <strong>Menunggu Pembayaran</strong>
                                 </h1>
                             @elseif ($data->status === 'completed')
-                                <h1 class="text-sm mt-2 text-info-500">
+                                <h1 class="text-sm mt-2 text-blue-500">
                                     <i class="fa-regular fa-info-circle mr-1"></i>
                                     <strong>Silahkan Check-in Kamar menggunakan QR Code</strong>
                                 </h1>   
